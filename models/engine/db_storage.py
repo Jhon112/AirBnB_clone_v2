ef53286dccaf355ba76099746dbd2c931abb8c0a
Jhon Arias <jhonarmont@hotmail.com>
2019-12-20 12:25:41 -0500

    change way classes are imported

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 """This is the database engine for mysql database"""
 from models.base_model import BaseModel, Base
-from models import User, State, City, Amenity, Place, Review
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import scoped_session
