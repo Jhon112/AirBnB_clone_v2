725536f6223067e25590165dab3eaf423855c6bc
Jhon Arias <jhonarmont@hotmail.com>
2020-01-23 00:39:23 -0500

    Add task 3. New route

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Starts a Flask web application
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbtn():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbtn():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    return "C {}".format(text.replace("_", " "))
+
+if __name__ == "__main__":
+    app.run(debug=True)
